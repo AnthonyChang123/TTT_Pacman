dea4ca9d67bde52bbbae276ebc13dcab81afce2f
AnthonyChang123 <fz7416lx@go.minnstate.edu>
2025-10-12 00:41:13 -0500

    Start
    I've created a few files to get us started, in the LoginPage.php I have comments and questions as well. These are things we'll need to look into

---
+++ LoginPage.php
@@ -0,0 +1,27 @@
+<?php
+include('header.php');
+
+?>
+
+<link rel="stylesheet" href="LoginForm.css">
+
+<!-- TODO: Connect this form to a PHP script (e.g., login_process.php) that validates user credentials from the database -->
+<!-- Look into "Front Controller", src folder, entity, http for sessions? -->
+<!-- Design question: Once successful, will the user be navigated to their account page or the home page?-->
+<!-- PHP Session and handling, need to brush up on this-->
+
+<form class="form_box" action="" method="POST">
+    <div class="TitleLogin"> Login Here </div>
+        <div>Username</div>
+        <input type="text"></input>
+        <div>Password</div>
+        <input type="password"></input>
+        <button type="submit" class="button">Login</button> 
+        <button class="button">Forgot Password</button>
+        <button class="button">Register</button>
+</form>
+
+
+<?php
+include('footer.php');
+?>
