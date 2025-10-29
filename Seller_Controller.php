dfdf3a82071141b93d4682d6a125119ebf9daf43
Marthe Odonelle <martha.njoya@my.metrostate.edu>
2025-10-29 17:47:21 -0500

    Add files via upload

---
+++ Seller_Controller.php
@@ -0,0 +1,36 @@
+<?php
+$db = require __DIR__ . '/Database.php';  
+require __DIR__ . '/UserModel.php';
+
+$userModel = new UserModel($db);  
+?>
+
+
+<?php
+session_start();
+
+if (isset($_POST['edit_profile'])) {
+  // Handle profile edit
+  echo "Editing profile...";
+}
+
+if (isset($_POST['delete_book'])) {
+  // Handle book deletion
+  echo "Deleting book...";
+}
+
+if (isset($_POST['save_book'])) {
+  // Handle book save
+  echo "Saving book...";
+}
+
+if (isset($_POST['edit_book'])) {
+  // Handle book edit
+  echo "Editing book...";
+}
+
+if (isset($_POST['post_book'])) {
+  // Handle book posting
+  echo "Posting book...";
+}
+?>
